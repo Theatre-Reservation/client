a1977669eecde150c2ff03d6e648aceff9e3c1e0
Shalani Kuruppu <shalanikuruppu08@gmail.com>
2024-10-22 10:31:03 +0530

    timeout added

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -50,7 +50,10 @@ const Header = () => {
       if (token) {
         setIsLoggedIn(true); // User is logged in
         if (!socketRef.current) {
-          socketRef.current = io('https://admin-service2-gshrdfcfftdccscx.canadacentral-01.azurewebsites.net/api/v1');
+          socketRef.current = io("https://admin-service2-gshrdfcfftdccscx.canadacentral-01.azurewebsites.net/api/v1", {
+            reconnectionAttempts: 5, // Retry for 5 attempts
+            timeout: 10000, // Set a timeout (e.g., 10 seconds)
+          });
           socketRef.current.on('newMovie', (movie) => {
             console.log('New movie received:', movie);
             setNotifications((prev) => [{ ...movie, type: 'movie' }, ...prev]);
